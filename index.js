9a8204285d1afa227f35b47b11bcbc52daf60ef8
vitaliiloboda <vitalii.loboda@gmail.com>
2018-06-09 19:32:36 +0300

    init

---
+++ index.js
@@ -0,0 +1,25 @@
+const ADMIN_LOGIN = 'admin';
+const ADMIN_PASSWORD = 'm4ngo1zh4ackz0r';
+
+const cancelAnswer = 'Отменено пользователем!';
+const wrongAnswer = 'Доступ запрещен!';
+const rightAnswer = 'Добро пожаловать!';
+
+let passwordAnswer;
+
+let loginAnswer = prompt('Введите логин:');
+
+if (loginAnswer == null) {
+  alert(cancelAnswer);
+} else if (loginAnswer !== ADMIN_LOGIN) {
+  alert(wrongAnswer);
+} else passwordAnswer = prompt ('Введите пароль:');
+
+if (passwordAnswer == null) {
+  alert(cancelAnswer);
+} else if (passwordAnswer !== ADMIN_PASSWORD) {
+  alert(wrongAnswer);
+} else alert(rightAnswer);
+
+
+
